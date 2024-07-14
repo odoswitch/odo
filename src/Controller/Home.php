59244145e23edc4169ca06b724218389aa97ea3b
David NGAMBA <dng@afritelec.fr>
2024-07-15 00:28:59 +0200

    1.0.2

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -5,12 +5,20 @@ namespace Odoswitch\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class Home extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(HttpClientInterface $client): Response
     {
-        return new Response("je suis ok David", 200);
+
+        $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
+
+        $issData = $response->toArray();
+
+        //dd($issData);
+        //return new Response("Je suis ok David", 200);
+        return $this->json($issData, 200);
     }
 }
