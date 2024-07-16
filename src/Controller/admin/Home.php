58fbb13da00f9e0230f0be730044af2ed6e83022
David NGAMBA <dng@afritelec.fr>
2024-07-16 02:00:07 +0200

    1.0.2

--- src/Controller/admin/Home.php
+++ src/Controller/admin/Home.php
@@ -9,13 +9,14 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class Home extends AbstractController
 {
-    #[Route('/dash', name: 'app_dash')]
+    #[Route('/admin/dash', name: 'app_dash')]
     public function index(HttpClientInterface $client): Response
     {
 
         $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
 
         $issData = $response->toArray();
+        return $this->render('admin/dash.html.twig', ['data' => $issData]);
 
         //dd($issData);
         //return new Response("Je suis ok David", 200);
